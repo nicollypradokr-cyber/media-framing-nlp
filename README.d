f96a2865be6e824b4eb2a0f501c911603ebae125
Prado Luz Nicolly <nicollyprado.kr@gmail.com>
2026-03-09 00:56:07 -0700

    Create README.d

---
+++ README.d
@@ -0,0 +1,22 @@
+# Media Framing Analysis with NLP
+
+This project analyzes how Donald Trump is represented in news coverage mentioning Jeffrey Epstein.
+
+Data sources
+- New York Times API
+- Fox News website
+
+Methods
+- Named Entity Recognition
+- Dependency Parsing
+- Sentiment Analysis
+- Keyword in Context (KWIC)
+- Narrative Frame Detection
+- Network Analysis
+
+Libraries
+- spaCy
+- pandas
+- seaborn
+- NetworkX
+- TextBlob
